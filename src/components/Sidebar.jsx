fb210fd0f264c12f0183d91b78140eded8886223
Giovanni Toledo <giovannitoledo@id.uff.br>
2023-04-02 00:09:28 -0300

    fix: components placement and avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import  { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -9,7 +10,7 @@ export function Sidebar() {
                 className={styles.cover}
                 src="https://images.unsplash.com/photo-1592609931095-54a2168ae893?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="" />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/DarkhnessXK.png"/>
+                <Avatar src="https://github.com/DarkhnessXK.png"/>
                 <strong>Giovanni Toledo</strong>
                 <span>Web Developer</span>
             </div>
